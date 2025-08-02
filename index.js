ec20737adf6eacfb3c93a0c31fd6a1a6cf0973fb
Christa Abou-Arraje <christa.arraj@gmail.com>
2025-08-02 18:32:20 -0400

    created the order section and fixed the footer

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => console.error('Error fetching header:', error));
 
 //footer
-        fetch('footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footerContainer').innerHTML = data;
-        })
-        .catch(error => console.error('Error fetching footer:', error));
+        // fetch('footer.html')
+        // .then(response => response.text())
+        // .then(data => {
+        //     document.getElementById('footerContainer').innerHTML = data;
+        // })
+        // .catch(error => console.error('Error fetching footer:', error));
     });
+    
